089d5a73d49d067ccfe569435d7db715e511c1d8
tim <tim@dockerz.net>
2012-04-24 10:20:51 +0000

    Fix test harness to support recursive UIs

--- Test.hs
+++ Test.hs
@@ -37,8 +37,10 @@ testAll = do
     set window [ containerBorderWidth := 10, windowTitle := "Test UI" ]
     vbox <- vBoxNew False 5
     let
-      addTest :: String -> e -> UI (IOE e) a -> IO ()
-      addTest title e uidef = do
+
+      -- This supports dynamic non-recursive uis
+      addEnvTest :: String -> e -> UI (IOE e) a -> IO ()
+      addEnvTest title e uidef = do
         let uig = uiGTK uidef
         button <- buttonNew
         set button [ buttonLabel := title ]
@@ -50,7 +52,18 @@ testAll = do
                L.putStrLn (DA.encode (uj_tojson (uiJSON uidef') v))
         containerAdd vbox button
 
-      addConstTest title uidef = addTest title () (ioFromConstUI uidef)
+      -- This supports constant potentially recursive uis
+      addConstTest :: String -> UI ConstE a -> IO ()
+      addConstTest title uidef = do
+        let uig = uiGTK (ioFromConstUI uidef)
+        button <- buttonNew
+        set button [ buttonLabel := title ]
+        dialog <- modalDialogNew () title uig [dialogOK,dialogReset,dialogCancel]
+        on button buttonActivated $ do
+           mr <- md_run dialog
+           maybeM mr $ \v -> do
+               L.putStrLn (DA.encode (uj_tojson (uiJSON uidef) v))
+        containerAdd vbox button
 
     addConstTest "StructTest" (mkUI :: UI ConstE StructTest)
     addConstTest "StructTest2" (mkUI :: UI ConstE StructTest2)
@@ -58,7 +71,7 @@ testAll = do
     addConstTest "[StructTest]" listTest
     addConstTest "StructTest3" (mkUI :: UI ConstE StructTest3)
     addConstTest "Expr" (mkUI :: UI ConstE Expr)
-    addTest "EnvStruct" "/tmp" envStructUI
+    addEnvTest "EnvStruct" "/tmp" envStructUI
        
     set window [ containerChild := vbox ]
     widgetShowAll window
